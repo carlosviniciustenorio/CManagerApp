1d99606321c28182b8acc196427bbea099bc1f8b
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2023-02-27 21:50:05 -0300

    controlando state

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -3,15 +3,21 @@ import 'dart:convert';
 import 'package:cmanagerapp/repositories/login_repository.dart';
 import 'package:http/http.dart' as http;
 
+import '../enums/home_state_enum.dart';
 import '../models/user_model.dart';
 
 class LoginController {
   final _loginRepository = LoginRepository();
+  HomeState state = HomeState.start;
+
   Future<bool> loginUser(User user) async {
+    state = HomeState.loading;
     var response = await _loginRepository.authenticate(user);
     if (response['sucesso']) {
+      state = HomeState.success;
       return true;
     } else {
+      state = HomeState.error;
       return false;
     }
   }
